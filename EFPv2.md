b706efb8c175b7d9faa12c4ab3db621234047a0a
mark-scherer <mark.s.scherer@vanderbilt.edu>
2018-06-21 16:18:29 -0500

    finalized EFPTv2 functionality

--- EFPv2.md
+++ EFPv2.md
@@ -1,18 +1,25 @@
 # External Feed Pathway Tester v2
-Tester for EVP without actual external feed. Tests SpatialMappingObserver, SpatialMappingManager, MeshManager, EFPDriver, Intersector and VoxelGridManager.
+Tester for EVP simulated external sensor. Tests SpatialMappingObserver, SpatialMappingManager, MeshManager, EFPDriver, Intersector and VoxelGridManager and Visualizer classes.
 
 ### Changes Since v1
 The following changes were made since EFP Tester v1:
-* Mesh/sensor-projection intersection: Now only non-occluded vertices are updated in voxel grid, previously all cached vertices were updated.
+* Mesh/sensor-projection intersection: only non-occluded vertices are updated in voxel grid, previously all cached vertices were updated.
 * Simulated external sensor: external feed input data simulated, previously updates were set to default value.
-* Better diagnostics: more accurate octree memory estimate, more detailed speed information.
-* Easier local build: more comphrehensive prefabs included in assets.
 
 ### Functionality
-EFPDriver pulls a list of all cached spatial mapping data from SpatialMappingObserver via SpatialMappingManager then MeshManager. EFPDriver then pushes spatial mapping data to Intersector, which calculates non-occluded mesh vertices and their projected sensor value. EFPDriver then pushes non-occlused vertex-value pairs to VoxelGridManager to be set in the voxel grid. TextControl pulls and displays process metadata.
-The app renders a wireframe of visible meshes as well as a panel of process diagnostics.
+Simulated data from a dummy external sensor positioned at the user's head is projected onto the mesh, with intersected vertices and their corresponding external feed values stored in a voxel grid. 
+Optionally intersected vertices can be rendered and colored according to their projected value. A diagnostic panel is also displayed.
+Eventually a real external sensor will be used and rendering will be done via the mesh texture itself.
 
-![picture alt](../master/imgs/EFPTesterv2Screenshot.jpg "screenshot")
+![picture alt](../master/imgs/EFPTesterv2screenshot2.jpg "screenshot")
+
+The app is driven by the Unity GameObject EFP, which has the following components: EFPDriver, SpatialMappingObserver, SpatialMappingManager, and MeshManager.
+Each cycle EFPDriver calls MeshManager to update a list of mesh vertices, which it does by polling SpatialMappingObserver and compiling all vertices from meshes it estimates as visible using an Intersector object.
+EFPDriver then uses its own Intersector object to determine which mesh vertices are actually visible and non-occluded, and then calculate their projected values from the simulated external sensor.
+Next EFPDriver maintains a voxel grid of those points and values using a VoxelGridManager object, which internally stores them in an Octree.
+Finally EFPDriver calls on a Visualizer object to render mesh vertices according to their project values. Optionally, MeshManager can also use its own Visualizer object to render bounding boxes around each individual mesh.
+
+The Unity GameObject Diagnostics drives the diagnostics panel. The DiagnosticsControl component polls various EFP components each cycle for metadata then displays them on the diagnostics panel.
 
 ### Building on Local Machine
 1. Import [Unity project assets](https://github.com/VUSE-Hololens/assets/tree/master/EFP%20Tester%20v2) and HoloToolkit package.
@@ -20,8 +27,11 @@ The app renders a wireframe of visible meshes as well as a panel of process diag
 3. Add following prefabs to project:
 	1. EFP
 	2. Diagnostics
-	3. TextControlContainer
-4. Within TextControlContainer/TextControl:
-	1. Set Text Container to Diagnostics/DiagnosticsText.
-	2. Set EFP Container to EFP.
-5. Build and deploy.
+	3. Lighting
+4. Within both EFP/EFPDriver and EFP/MeshManager:
+	1. Set DefaultMaterial to Unity's built-in default material.
+	2. Assign all other inspector variables as desired.
+5. Within Diagnostics/DiagnosticsControlContainer/DiagnosticsControl:
+	1. Set DiagnosticsText (field) to DiagnosticsText (GameObject)
+	2. Set EFPContainer (field) to EFP (GameObject)
+6. Build and deploy.
---
+++ archive/IntersectorOld2.cs
@@ -0,0 +1,261 @@
+﻿/// Intersector
+/// Class for calculating resultant PointValues when 2D Raster is project along a Frustum at a Mesh.
+/// Content:
+/// PointValue (struct)
+/// ViewVector (struct)
+/// Frustum (struct)
+/// OcclusionCell (struct)
+/// Intersector (class)
+
+/// NOTE: Tried to make Intersector:Intersection method generic so it was ambivalent to type of img. 
+/// This caused strange Unity build errors when accessing and mutating values within cells of ocGrid.
+/// Currently set to byte, if img data type changes will have to manually change code.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generic spatially-aware values.
+/// </summary>
+public struct PointValue<T>
+{
+    public Vector3 Point { get; private set; }
+    public T Value { get; private set; }
+
+    public PointValue(Vector3 myPoint, T myValue)
+    {
+        Point = myPoint;
+        Value = myValue;
+    }
+
+    /// <summary>
+    /// Same as constructor, except no new memory allocation.
+    /// </summary>
+    public void Update(Vector3 newPoint, T newValue)
+    {
+        Point = newPoint;
+        Value = newValue;
+    }
+}
+
+/// <summary>
+/// Vector for relative angle of (x,y,z) vector relative to 2D plane. Vector must originate at origin.
+/// For 2D plane = XY plane, positive Z is forwards, 2D plane coordinates i,j:
+/// Theta (i): angle of vector with YZ plane
+/// Phi (j): angle of vector with XZ plane
+/// </summary>
+public struct ViewVector
+{
+    public double Theta { get; private set; }
+    public double Phi { get; private set; }
+
+    public ViewVector(double myTheta, double myPhi)
+    {
+        Theta = myTheta;
+        Phi = myPhi;
+    }
+
+    /// <summary>
+    /// Creates ViewVector from spatialVector.
+    /// Note: spatialVector MUST originate from origin.
+    /// </summary>
+    public ViewVector(Vector3 spatialVector)
+    {
+        Theta = Intersector.RadToDeg(Intersector.AdjAtanTheta(spatialVector.x, spatialVector.z));
+        Phi = Intersector.RadToDeg(Intersector.AdjAtanPhi(spatialVector));
+    }
+}
+
+/// <summary>
+/// Spatially-aware view field of a sensor.
+/// FOV defines full angle of frustum field of view, centered at transform. 
+/// </summary>
+public struct Frustum
+{
+    public Transform Transform;
+    public ViewVector FOV;
+
+    public Frustum(Transform myTransform, ViewVector myFOV)
+    {
+        Transform = myTransform;
+        FOV = myFOV;
+    }
+}
+
+/// <summary>
+/// Cell of Occlusion grid using closest-in-grid occlusion approximation strategy.
+/// </summary>
+public struct OcclusionCell<T>
+{
+    public Vector3 closest;
+    public float distance;
+    public T value;
+    public bool nullCell;
+
+    public static OcclusionCell<T>[,] OcGrid(Vector2 size)
+    {
+        OcclusionCell<T>[,] grid = new OcclusionCell<T>[(int)size.x, (int)size.y];
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.y; j++)
+                grid[i, j].nullCell = true;
+        }
+        return grid;
+    }
+}
+
+
+/// <summary>
+/// Calculates resultant PointValues when 2D Raster is projected along a Frustum at a Mesh.
+/// Mesh represented by list of vertices.
+/// NOTE: does not currently account for occlusion.
+/// </summary>
+public class Intersector : HoloToolkit.Unity.Singleton<Intersector>
+{
+    /// <summary>
+    /// Metadata: how many vertices were in passed Frustum for last call to Intersection().
+    /// </summary>
+    public int CheckedVertices { get; private set; }
+
+    /// <summary>
+    /// Metadata: how many vertices were in passed Frustum for last call to Intersection().
+    /// </summary>
+    public int VerticesInView { get; private set; }
+
+    /// <summary>
+    /// Metadata: how many vertices were in passed Frustum and non-occluded for last call to Intersection().
+    /// </summary>
+    public int NonOccludedVertices { get; private set; }
+
+    // controls for occlusion grid size calculations
+    public static float ObjSize = 0.1f; // m
+    public static float ObjDistance = 5f; //m
+
+    /// pre-declarations of variables in Intersection() for memory efficiency
+    private int i, j, iOc, jOc, iImg, jImg;
+
+    // new Intersection
+    /// <summary>
+    /// Calculates resultant PointValues when 2D Raster (img) is projected along Frustum at Mesh (vertices).
+    /// Mesh represented by list of vertices.
+    /// NOTE: projection's FOV should be full FOV angles, not half-angles.
+    /// </summary>
+    public List<PointValue<byte>> Intersection(Frustum projection, ref List<Vector3> vertices, ref byte[,] raster)
+    {
+        /// setup
+        List<PointValue<byte>> Results = new List<PointValue<byte>>();
+        VerticesInView = 0;
+        Vector2 ImgSize = new Vector2();
+        ImgSize.x = raster.GetLength(0);
+        ImgSize.y = raster.GetLength(1);
+
+        Vector2 OcGridSize = GridSize(projection.FOV);
+        OcclusionCell<byte>[,] ocGrid = OcclusionCell<byte>.OcGrid(OcGridSize);
+
+        /// try each vertex
+        for (i = 0; i < vertices.Count; i++)
+        {
+            /// calculate position vector (world space)
+            Vector3 Pworld = Vector(projection.Transform.position, vertices[i]);
+
+            /// convert position vector (world space) to position vector (Frustum space)
+            Vector3 Plocal = projection.Transform.InverseTransformVector(Pworld);
+
+            /// convert position vector (Frustum space) to view vector (Frustum space)
+            ViewVector Vlocal = new ViewVector(Plocal);
+
+            /// check if view vector is within Frustum FOV
+            if (Math.Abs(Vlocal.Theta) < projection.FOV.Theta / 2.0 &&
+                Math.Abs(Vlocal.Phi) < projection.FOV.Phi / 2.0)
+            {
+                VerticesInView++;
+
+                /// map view vector to occlusion grid
+                iOc = (int)(OcGridSize.x / 2 + OcGridSize.x * (Vlocal.Theta / projection.FOV.Theta));
+                jOc = (int)(OcGridSize.y / 2 + OcGridSize.y * (Vlocal.Phi / projection.FOV.Phi));
+
+                if (ocGrid[iOc, jOc].nullCell || Pworld.magnitude < ocGrid[iOc, jOc].distance)
+                {
+                    /// map view vector to Img grid
+                    iImg = (int)(ImgSize.x / 2 + ImgSize.x * (Vlocal.Theta / projection.FOV.Theta));
+                    jImg = (int)(ImgSize.y / 2 + ImgSize.y * (Vlocal.Phi / projection.FOV.Phi));
+
+                    ocGrid[iOc, jOc].closest = vertices[i];
+                    ocGrid[iOc, jOc].distance = Pworld.magnitude;
+                    ocGrid[iOc, jOc].value = raster[iImg, jImg];
+                    ocGrid[iOc, jOc].nullCell = false;
+                }
+            }
+        }
+
+        // add vertices in occluded grid
+        for (i = 0; i < OcGridSize.x; i++)
+        {
+            for (j = 0; j < OcGridSize.y; j++)
+                if (!ocGrid[i, j].nullCell)
+                    Results.Add(new PointValue<byte>(ocGrid[i, j].closest, ocGrid[i, j].value));
+        }
+
+        CheckedVertices = vertices.Count;
+        NonOccludedVertices = Results.Count;
+        return Results;
+    }
+
+    /// <summary>
+    /// Returns vector FROM point1 TO point2.
+    /// </summary>
+    public Vector3 Vector(Vector3 point1, Vector3 point2)
+    {
+        return point2 - point1;
+    }
+
+    /// <summary>
+    /// Converts radians to degrees.
+    /// </summary>
+    public static double RadToDeg(double rad)
+    {
+        return rad * (180.0 / Math.PI);
+    }
+
+    public static double DegToRad(double deg)
+    {
+        return deg * (Math.PI / 180.0);
+    }
+
+    /// <summary>
+    /// Calculates arctan (in radians) so that resultant angle is between -pi and pi.
+    /// </summary>
+    public static double AdjAtanTheta(double numerator, double denominator)
+    {
+        if (denominator < 0 && numerator < 0) /// Q3
+            return -Math.PI / 2.0 - Math.Atan(denominator / numerator);
+        if (numerator > 0 && denominator < 0) /// Q4
+            return Math.PI / 2.0 + Math.Atan(Math.Abs(denominator) / numerator);
+        return Math.Atan(numerator / denominator);
+    }
+
+    /// <summary>
+    /// Calculates arctan (in radians) always in reference to plane of numerator.
+    /// </summary>
+    public static double AdjAtanPhi(Vector3 spatialVector)
+    {
+        Vector3 proj = new Vector3(spatialVector.x, 0, spatialVector.z);
+        float mag = proj.magnitude;
+        return Math.Atan(spatialVector.y / mag);
+    }
+
+    /// <summary>
+    /// Determines the required grid size given hard-coded parameters for occlusion approximation via closest in grid.
+    /// </summary>
+    public static Vector2 GridSize(ViewVector FOV)
+    {
+        float totalViewSizeX = 2.0f * ObjDistance * (float)Math.Tan(DegToRad(FOV.Theta) / 2.0);
+        float totalViewSizeY = 2.0f * ObjDistance * (float)Math.Tan(DegToRad(FOV.Phi) / 2.0);
+
+        Vector2 dims = new Vector2();
+        dims.x = (int)Math.Round(totalViewSizeX / ObjSize);
+        dims.y = (int)Math.Round(totalViewSizeY / ObjSize);
+        return dims;
+    }
+}
